6926d052230d608bc9961d5e30d6bae688bb207f
Selector <973663+Selectorrr@users.noreply.github.com>
2025-06-22 18:49:01 +0300

    теперь формат wav поумолчанию и фоаги передаются без аргумента true/false

--- entrypoint.py
+++ entrypoint.py
@@ -20,18 +20,18 @@ def get_def_providers():
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
 
-    parser.add_argument('--ext', type=str, default="ogg", help='Формат результирующего ауидо')
+    parser.add_argument('--ext', type=str, default="wav", help='Формат результирующего ауидо')
     parser.add_argument('--tone_sample_len', type=int, default=7500, help='Длина сэмпла голоса')
     parser.add_argument('--api_key', type=str, help='Ваш ключ доступа к api', required=True)
     parser.add_argument('--batch_size', type=int, default=20, help='Размер батча')
     parser.add_argument('--n_jobs', type=int, default=None, help='Количество воркеров')
     parser.add_argument('--csv_delimiter', type=str, default=',', help='Разделитель, использующийся в csv.')
-    parser.add_argument('--is_strict_len', type=bool, default=False,
+    parser.add_argument('--is_strict_len', action='store_true',
                         help='Должна ли длина аудио строго совпадать с оригиналом')
-    parser.add_argument('--is_use_voice_len', type=bool, default=True,
+    parser.add_argument('--is_use_voice_len', action='store_true',
                         help='Если строгая длина и is_use_voice_len=true то в качестве продолжительности будет '
                              'учитываться конец речи в аудио, а не длина всего аудио')
-    parser.add_argument('--is_respect_mos', type=bool, default=False,
+    parser.add_argument('--is_respect_mos', action='store_true',
                         help='Нужно ли учитывать качество звука для построения сэмпла голоса')
     parser.add_argument('--providers', nargs='+', default=get_def_providers(),
                         help='Список провайдеров для выполнения (ONNX runtime)')
